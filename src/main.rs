e713539e7e2dd61531ba034b24fe2a1b9e433a91
wakewakame <0214.taiki@gmail.com>
2024-01-04 18:53:39 +0900

    入出力が別スレッドでも動くことを確認

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod jsruntime;
 
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    let mut runtime: Box<dyn jsruntime::ScriptRuntime> = Box::new(jsruntime::JsRuntime::new());
-    _ = (&mut *runtime).compile(
-        r#"
+    let (input_tx, input_rx) = mpsc::channel::<Vec<f64>>();
+    let (output_tx, output_rx) = mpsc::channel::<Vec<f64>>();
+    let th = thread::spawn(move || {
+        let mut runtime: Box<dyn jsruntime::ScriptRuntime> = Box::new(jsruntime::JsRuntime::new());
+        _ = (&mut *runtime).compile(
+            r#"
 			console.log("hello world");
 			let sum = 0;
 			(input, output) => {
@@ -13,9 +19,16 @@ fn main() {
 				});
 			};
         "#,
-    );
-    let mut output = vec![0f64; 4];
-    _ = (&mut *runtime).process(&vec![0f64, 1f64, 2f64, 5f64], &mut output);
+        );
+        let input = input_rx.recv().unwrap();
+        let mut output = vec![0f64; 4];
+        _ = (&mut *runtime).process(&input, &mut output);
+        let _ = output_tx.send(output);
+    });
+    let input = vec![0f64, 1f64, 2f64, 5f64];
+    let _ = input_tx.send(input);
+    let _ = th.join();
+    let output = output_rx.recv().unwrap();
     for v in output.iter() {
         println!("{}", v);
     }
